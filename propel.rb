8cc016595ff5634089d071375e605bd285264b22
Marcus Mu <chunkhang@gmail.com>
2017-06-27 03:08:26 +0800

    Updated formulae with url and sha256

--- propel.rb
+++ propel.rb
@@ -3,8 +3,8 @@ require "formula"
 class Propel < Formula
   desc "Propel your commit because you can"
   homepage "https://github.com/chunkhang/homebrew-propel"
-  url ""
-  sha256 ""
+  url "https://github.com/chunkhang/homebrew-propel/archive/1.0.0.tar.gz"
+  sha256 "30c3fcb5cc7f9ede1dc1e00545a09fb38afc5ff23098b14e81b877bd7a929b23"
 
   def install
   	bin.install "bin/propel"
